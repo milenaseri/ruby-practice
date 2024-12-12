af88d0543387dcfed7f77f1a90ad7597d710337c
Milena Seri <milenaseri@outlook.com>
2024-12-12 19:19:50 -0300

    refactor(structure): reorganize code into topic-based directories

--- basics/strings/daffy_duckify.rb
+++ basics/strings/daffy_duckify.rb
@@ -2,10 +2,10 @@
 
 print 'Thtring, pleathe!: '
 user_input = gets.chomp
-user_input.downcase!
+# user_input.downcase!
 
-if user_input.include? 's'
-  user_input.gsub!(/s/, 'th')
+if user_input.downcase.include? 's', 'S'
+  user_input.gsub!(/s/, 'th', /S/, 'TH')
 else
   puts 'Nothing to do here!'
 end
